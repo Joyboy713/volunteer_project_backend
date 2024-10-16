c68b8c137135f542322b2f932d1213c82edd3646
tanhtran <tanhtran246@gmail.com>
2024-10-15 19:11:05 -0500

    server fix

--- server.js
+++ server.js
@@ -1,33 +1,20 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log("Connected to MongoDB"))
-    .catch(err => console.error("MongoDB connection error:", err));
-
-
-// Required to use __dirname with ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import notificationRoutes from './src/routes/notifications.js';
+import volunteerHistoryRoutes from './src/routes/volunteerHistory.js';
 
 const app = express();
+const port = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
-app.use(express.json());  // To parse JSON data
+app.use(express.json());
 
-// Serve static files from the "public" directory
-app.use('/public', express.static(path.join(__dirname, 'public')));
-
-// Set up the server
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Notification route
+app.use('/api/notifications', notificationRoutes);
+// Volunteer History Routes
+app.use('/api/volunteerHistory', volunteerHistoryRoutes); 
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
